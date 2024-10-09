e7047014c427e74eaf6fa8add50896f9cd78f592
nimish <nmagre@broadinstitute.org>
2024-10-09 20:41:33 +0000

    final regular lr model 1

--- cellarium/workflows/example/components.py
+++ cellarium/workflows/example/components.py
@@ -4,11 +4,11 @@ from kfp.dsl import *
 from typing import *
 
 
-@dsl.component(base_image="us-central1-docker.pkg.dev/broad-dsde-methods/cellarium-ai/cellarium-ml@sha256:0722fcc9364f86e826bfdc1aa1f33149e37c0c615043096a82550317c8f4aeec", 
+@dsl.component(base_image="us-central1-docker.pkg.dev/dsp-cell-annotation-service/cas-services-cicd/cas-pytorch-cuda-pipeline-dev:1.4.5-alpha.1", 
                packages_to_install=[
+                   "anndata==0.10.9",
                    "git+https://github.com/cellarium-ai/cellarium-ml.git@lr_with_sequential_lr_scheduler",
                    "gcsfs",
-                   "tensorboard",
                    ])
 def base_model_lr_run_final(config: str = "gs://cellarium-file-system/ml-configs/Supervised_cell_classification/Base_model_regular_LR/Base_model_regular_lr.yaml",
                 ):
