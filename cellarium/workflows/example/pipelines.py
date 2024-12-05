27fece15df8f441c3d4bb7f234f484ba3e20b1ce
nimish <nmagre@broadinstitute.org>
2024-12-05 22:59:46 +0000

    trial for alph=0

--- cellarium/workflows/example/pipelines.py
+++ cellarium/workflows/example/pipelines.py
@@ -4,14 +4,14 @@ from cellarium.workflows.example import components
 
 
 @dsl.pipeline()
-def model_5_pipeline_lrexp_human_alpha_half(component_1_config: str = "gs://cellarium-file-system/ml-configs/lrexp_human_train/Model_variation_5/model_5_alpha_half_config.yaml"):
+def model_5_pipeline_lrexp_human_alpha_zero_final(component_1_config: str = "gs://cellarium-file-system/ml-configs/lrexp_human_train/Model_variation_5/model_5_alpha_one_config.yaml"):
     """
     KFP pipeline to run tdigest train pipeline.
 
     """
     component_job_1 = kfp_helpers.create_job(
-        component_func=components.model_5_lrexp_human_alpha_half,
-        display_name="model_5_lrexp_human_alpha_half",
+        component_func=components.model_5_lrexp_human_alpha_zero_final,
+        display_name="model_5_lrexp_human_alpha_zero_final",
         replica_count=1,
         machine_type="n1-highmem-16",
         accelerator_type = "NVIDIA_TESLA_T4",
@@ -25,4 +25,4 @@ def model_5_pipeline_lrexp_human_alpha_half(component_1_config: str = "gs://cell
 
 
 if __name__ == '__main__':
-    model_5_pipeline_lrexp_human_alpha_half()
+    model_5_pipeline_lrexp_human_alpha_zero_final()
