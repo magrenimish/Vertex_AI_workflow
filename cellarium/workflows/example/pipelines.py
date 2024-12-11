64fe1d05f487f3b7a1af158d26cd3c41302f48a4
nimish <nmagre@broadinstitute.org>
2024-12-11 21:35:53 +0000

    base_model_pp_hop_score_calculation_trial_1_final

--- cellarium/workflows/example/pipelines.py
+++ cellarium/workflows/example/pipelines.py
@@ -4,18 +4,18 @@ from cellarium.workflows.example import components
 
 
 @dsl.pipeline()
-def base_model_no_pp_hop_score_calculation_trial_2_final(component_1_config: str = "gs://cellarium-file-system/ml-configs/lrexp_human_validation/Base_model_regular_lr_no_pp/Base_model_no_pp_prediction_config.yaml"):
+def base_model_pp_hop_score_calculation_trial_1_final(component_1_config: str = "gs://cellarium-file-system/ml-configs/lrexp_human_validation/Base_model_regular_lr_pp/Base_model_pp_prediction_config.yaml"):
     """
     KFP pipeline to run tdigest train pipeline.
 
     """
     component_job_1 = kfp_helpers.create_job(
-        component_func=components.base_model_no_pp_hop_score_calculation_trial_2,
-        display_name="base_model_no_pp_hop_score_calculation_trial_2_final",
+        component_func=components.base_model_pp_hop_score_calculation_trial_1,
+        display_name="base_model_pp_hop_score_calculation_trial_1_final",
         replica_count=5,
         machine_type="n1-highmem-32",
-        #accelerator_type = "NVIDIA_TESLA_T4",
-        accelerator_count=1,
+        accelerator_type = "NVIDIA_TESLA_T4",
+        accelerator_count=2,
         config=component_1_config
     )
 
