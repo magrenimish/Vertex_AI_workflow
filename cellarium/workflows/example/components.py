6a9263ca231b9544012be3acf2517d737b268157
nimish <nmagre@broadinstitute.org>
2024-12-12 23:29:58 +0000

    model_variation_5_alpha_one_normalized_hop_score_calculation

--- cellarium/workflows/example/components.py
+++ cellarium/workflows/example/components.py
@@ -10,7 +10,7 @@ from typing import *
                    "git+https://github.com/cellarium-ai/cellarium-ml.git@logistic_regression_variation_5",
                    "gcsfs",
                    ])
-def model_variation_5_alpha_half_normalized_hop_score_calculation(config: str = "gs://cellarium-file-system/ml-configs/lrexp_human_validation/Model_5_alpha_half_normalized/model_5_alpha_half_normalization_prediction_config.yaml",
+def model_variation_5_alpha_one_normalized_hop_score_calculation(config: str = "gs://cellarium-file-system/ml-configs/lrexp_human_validation/Model_5_alpha_one_normalized/model_5_alpha_one_normalization_prediction_config.yaml",
                 ):
     """
     Test Example component
